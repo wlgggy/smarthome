f2e4be3f3e359c0900c009ab7977f33d944e6794
wlgggy <doyouregretme@naver.com>
2025-05-30 15:58:25 +0900

    ★[update] : main code★

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:smarthome/ui/splash_screen.dart';
+import 'package:smarthome/ui/view/home/home_view.dart';
 import 'package:smarthome/ui/view/tab_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool darkState = true;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +23,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        primaryColor: Color.fromARGB(255, 112, 112, 112),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueAccent,
+          error: darkState ? Colors.black : Colors.white,
+
+
+        ),
       ),
       home: LayoutView(),
     );
